8bb7f830f16a4f12fcb8e31ce197a39820551967
Brandt Kurowski <bkurowski@uceap.universityofcalifornia.edu>
2025-06-06 17:07:52 +0000

    consolidate build scripts

--- .devcontainer/onCreate.sh
+++ .devcontainer/onCreate.sh
@@ -20,7 +20,35 @@ rm files.tar.gz
 sudo find web/sites/default/files -type d -exec chmod g+ws {} +
 sudo find web/sites/default/files -type f -exec chmod g+w {} +
 
-# Set up infrastructure tooling
+# Install Terraform
 # TODO move this into base image
-build/install-terraform.sh
-build/install-azure-cli.sh
+
+sudo apt-get install -y gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+  gpg --dearmor | \
+  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+  --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+  --fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+  sudo tee /etc/apt/sources.list.d/hashicorp.list > /dev/null
+sudo apt update
+sudo apt-get install -y terraform
+
+# Install Azure CLI
+# TODO move this into base image
+
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc | \
+  gpg --dearmor | sudo tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+sudo chmod go+r /etc/apt/keyrings/microsoft.gpg
+AZ_DIST=$(lsb_release -cs)
+echo "Types: deb
+URIs: https://packages.microsoft.com/repos/azure-cli/
+Suites: ${AZ_DIST}
+Components: main
+Architectures: $(dpkg --print-architecture)
+Signed-by: /etc/apt/keyrings/microsoft.gpg" | sudo tee /etc/apt/sources.list.d/azure-cli.sources > /dev/null
+sudo apt-get update
+sudo apt-get install -y azure-cli
